5ef0365be036ae3b0317de46d8d2941ea2167a08
Daksh Patel <daksh.patel@organimi.com>
2024-09-06 23:48:35 -0400

    Old habits, die hard! created tsx file instead of jsx

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-import { useState } from 'react';
-import './App.css';
-import { ATTRIBUTE_LIST, CLASS_LIST, SKILL_LIST } from './consts.js';
+import "./App.css";
 import CharacterSheetWrapper from "./CharacterSheet";
 
-
 function App() {
-  const [num, setNum] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
         <h1>React Coding Exercise</h1>
       </header>
       <section className="App-section">
-        <CharacterSheetWrapper/>
+        <CharacterSheetWrapper />
       </section>
     </div>
   );
